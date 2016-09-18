818eb436d0f1ce19af405553c73d90bcebb13a28
Michael Rawlings <ml.rawlings@gmail.com>
2016-09-18 02:21:28 -0700

    remove console.log

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 
-console.log('hello world')
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
